66df9ececda9e6ddecb4e5404f92c4dc482df574
Glakuta <glakuta@gmail.com>
2023-03-12 21:11:38 +0100

    Create navbar and hero section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Tilt from 'react-tilt'
+import { motion } from 'framer-motion'
+
+import { styles } from '../styles'
 const About = () => {
   return (
     <div>About</div>
